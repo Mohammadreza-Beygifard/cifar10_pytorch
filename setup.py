7e2af028e75c0e7d90dd22d4595693e90daed3d5
Mohammadreza Beygifard <samanbeygifard73@gmail.com>
2023-10-29 14:30:12 +0100

    Add setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="cifar_10_pytorch",
+    version="1.0",
+    description="Using different models on CIFAR-10 dataset",
+    author="MohammadReza Beygifard",
+    author_email="samanbeygifard73@gmail.com",
+    install_requires=[
+        "numpy==1.24.3",
+        "pandas==2.0.3",
+        "matplotlib==3.7.2",
+        "beepy==1.0.7",
+    ],
+)
