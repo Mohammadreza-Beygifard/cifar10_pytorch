1cbc4515673f0b940cb23a311ad4be379dc0fcef
Mohammadreza Beygifard <samanbeygifard73@gmail.com>
2023-11-13 22:59:22 +0100

    Add save and load methods

--- inference_pipeline/check_performance.py
+++ inference_pipeline/check_performance.py
@@ -1,4 +1,25 @@
 import torch
+import os
+from datetime import datetime
+
+
+def save_model_state(state_dict_epoch: dict):
+    if not os.path.exists("./model_state"):
+        os.mkdir("./model_state")
+
+    current_date = datetime.now().strftime("%Y-%m-%d_%H-%M")
+    filename = f"./model_state/model_{current_date}.pt"
+
+    torch.save(state_dict_epoch["state_dict"], filename)
+
+
+def load_model_state():
+    if os.path.exists("./model_state"):
+        files = os.listdir("./model_state")
+        if len(files) > 0:
+            latest_file = max(files, key=os.path.getctime)
+            return torch.load(f"./model_state/{latest_file}")
+    return None
 
 
 def check_performance(model, test_dataset):
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from data_pipeline.data_pipeline import get_cifar10, get_train_data
 from train_pipeline.train_pipeline import plot_loss_accuracy, run_train
 from models.vgg import vgg_a
+from datetime import datetime
 
 
 if __name__ == "__main__":
     # create an example to test the plot function
     # plot_loss_accuracy(loss, accuracy, epochs)
-    original_train_dataset, test_dataset = get_cifar10()
-    train_dataset = get_train_data()
-    model = vgg_a()
-    run_train(model, train_dataset, 2)
+    # original_train_dataset, test_dataset = get_cifar10()
+    # train_dataset = get_train_data()
+    # model = vgg_a()
+    # run_train(model, train_dataset, 2)
     # print(model.eval())
     # train_model();
+    pass
